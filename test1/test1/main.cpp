c5d6757014b0b1e90318a2bcb42e1e5a44317054
sms <secrue1u@gmail.com>
2018-05-02 16:07:33 +0900

    test2

--- test1/test1/main.cpp
+++ test1/test1/main.cpp
@@ -12,11 +12,21 @@
 	 PORTD = 0xFF;
 
 	 /* Replace with your application code */
+
+	 int i;
+	 /* Replace with your application code */
 	 while (1)
 	 {
-		 PORTD = 0x00;
-		 _delay_ms(500);
-		 PORTD = 0XFF;
-		 _delay_ms(500);
+		 for(i = 0; i > 7; i++)
+		 {
+			 PORTD = ~(1<< i);
+			 _delay_ms(500);
+			 
+		 }
+		 for(i = 0; i > 7; i++)
+		 {
+			 PORTD = ~(0x80 >> i);
+			 _delay_ms(500);
+		 }
 	 }
  }
